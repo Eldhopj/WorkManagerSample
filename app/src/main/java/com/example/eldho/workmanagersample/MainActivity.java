aa86d87079b059ed2b40ec48fc225c8b1b63c79e
eldhopj <eldho@live.in>
2019-08-20 17:18:11 +0530

    Added constraints and code improvements

--- app/src/main/java/com/example/eldho/workmanagersample/MainActivity.java
+++ app/src/main/java/com/example/eldho/workmanagersample/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.eldho.workmanagersample;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
+import androidx.work.Constraints;
+import androidx.work.NetworkType;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
@@ -16,6 +19,8 @@ import butterknife.OnClick;
 
 /**
  * NOTE : WorkManager is intended for tasks that are not required to run immediately , and required to run reliably even if the app exits or the device restart
+ * Where to use which : https://1.bp.blogspot.com/-0o3ndTbxAHQ/W8UdDaO6iaI/AAAAAAAAF0w/o-p-Ghm0xNsdgBX9ohELYO3UMB2ZO_FMQCLcBGAs/s1600/image1.png
+ *                      https://android-developers.googleblog.com/2018/10/modern-background-execution-in-android.html
  *
  * Worker : The work needed to be done is defined here
  * WorkRequest : it define work, like which worker class is going to be executed.
@@ -29,33 +34,51 @@ public class MainActivity extends AppCompatActivity {
     Button oneTimeWork;
     @BindView(R.id.workStatus)
     TextView workStatus;
+    private static final String TAG = "MainActivity";
+    private WorkManager mWorkManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        workStatusListener();
+        mWorkManager = WorkManager.getInstance(getApplicationContext());
+        workInfoListener();
 
     }
 
     private OneTimeWorkRequest onetimeWorkReq() {
         OneTimeWorkRequest workRequest = new OneTimeWorkRequest
                 .Builder(WorkerClass.class)
+                .setConstraints(getConstraint())
                 .build();
         return workRequest;
     }
 
+    // creates constraints for job to run , the job will run when the constraints satisfies
+    private Constraints getConstraint() {
+        Constraints constraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.METERED)
+                .setRequiresCharging(true)
+                .build();
+        return constraints;
+    }
+
     // Observe the work changes
-    private void workStatusListener () {
-        WorkManager.getInstance(getApplicationContext())
-                .getWorkInfoByIdLiveData(onetimeWorkReq().getId())
+    private void workInfoListener() {
+        mWorkManager.getWorkInfoByIdLiveData(onetimeWorkReq().getId())
                 .observe(this, new Observer<WorkInfo>() {
                     @Override
                     public void onChanged(WorkInfo workInfo) {
+                        // If there are no matching work info, do nothing
+                        if (workInfo == null) {
+                            return;
+                        }
                         // workInfo : live data where the status of work contains
                         workStatus.setText("Status : " + workInfo.getState().name());
+                        if (workInfo.getState() == WorkInfo.State.SUCCEEDED) {
+                            Log.d(TAG, "onChanged: " + "succeed");
+                        }
                     }
                 });
     }
@@ -63,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.oneTimeWork)
     public void onClick() {
         // we need work manager to perform the work
-        WorkManager.getInstance(getApplicationContext()).enqueue(onetimeWorkReq());
+        mWorkManager.enqueue(onetimeWorkReq());
     }
 
 
--- app/src/main/java/com/example/eldho/workmanagersample/WorkerClass.java
+++ app/src/main/java/com/example/eldho/workmanagersample/WorkerClass.java
@@ -1,6 +1,7 @@
 package com.example.eldho.workmanagersample;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.work.Worker;
@@ -8,6 +9,7 @@ import androidx.work.WorkerParameters;
 
 public class WorkerClass extends Worker {
     private Context mContext;
+    private static final String TAG = "WorkerClass";
     public WorkerClass(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
         mContext = context;
@@ -18,9 +20,17 @@ public class WorkerClass extends Worker {
     /**Inside this func we do our work that we need to exec in background*/
     public Result doWork() {
         // Work here is to display a notification
-        NotificationUtils.createNotifications(mContext,"Success","work done successfully");
-        return Result.success(); // if work is success
-//        return Result.failure(); // if work is failture
-//        return Result.retry() // if we want to retry the task
+        try {
+            NotificationUtils.createNotifications(mContext, "Success", "work done successfully");
+            return Result.success(); // if work is success
+        } catch (Throwable throwable) {
+
+            // Technically WorkManager will return Result.failure()
+            // but it's best to be explicit about it.
+            // Thus if there were errors, we're return FAILURE
+            Log.e(TAG, "Error applying blur", throwable);
+            return Result.failure();
+//          return Result.retry(); // if we want to retry the task
+        }
     }
 }
